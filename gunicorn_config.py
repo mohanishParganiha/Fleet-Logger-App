9a9ebdd0fdf4907a9be056e1c0dbe43cc919e37b
mohanishParganiha <harshparganiha@gmail.com>
2026-03-02 16:10:33 +0530

    added Gunicorn and gunicorn settings to django and docker compose files

---
+++ gunicorn_config.py
@@ -0,0 +1,32 @@
+"""this is gunicorn config file"""
+import multiprocessing
+
+# server socket
+bind = "0.0.0.0:8000"
+
+# workers
+workers = multiprocessing.cpu_count()*2+1
+worker_class = "sync"
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# logging
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+
+# processing name
+proc_name = "fleet_logger_api"
+
+# server mechanics
+deamon = False
+pidfile = None
+mask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# ssl well add later
+# keyfile = None
+# certfile = None
